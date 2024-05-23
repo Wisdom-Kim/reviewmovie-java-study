526e6ac524ad35ea42c69606c85f6e7c06c77e78
HYH <dihexyldecyl@gmail.com>
2024-05-23 18:47:30 +0900

    #Feat: encoding filter 추가

---
+++ src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,33 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpFilter;
+
+@WebFilter(urlPatterns = {"*.do"},
+			initParams = {@WebInitParam(name = "charset", value = "UTF-8")})
+public class EncodingFilter extends HttpFilter implements Filter {
+
+	String charset;
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		charset = fConfig.getInitParameter("charset");
+	}
+    
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding(charset);
+		
+		chain.doFilter(request, response);
+		
+		response.setCharacterEncoding(charset);
+	}
+
+
+}
