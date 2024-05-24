4943153db8fc29aab3712e33344fc2001d5c45a1
wisdom <52325199+Wisdom-Kim@users.noreply.github.com>
2024-05-24 10:10:48 +0900

    Feat: DTO 추가
    - Movie, Rating, USer DTO 추가

--- src/main/java/dto/MovieDTO.java
+++ src/main/java/dto/MovieDTO.java
@@ -1,5 +1,38 @@
 package dto;
 
+import domain.Movie;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class MovieDTO {
 
+//	private boolean likeStatement;
+//	private int userId;
+//	private int reviewId;
+//	
+//	@Builder
+//	public MovieDTO(boolean likeStatement,int userId,int reviewId) {
+//		
+//		this.likeStatement = likeStatement;
+//		this.userId = userId;
+//		this.reviewId = reviewId;
+//	}
+//	
+//	public static Movie toEntity(MovieDTO movieDTO) {
+//		return Movie.builder()
+//					.deptno(deptDTO.getDeptno())
+//					.dname(deptDTO.getDname())
+//					.loc(deptDTO.getLoc())
+//					.build();
+//	}
+	
 }
--- src/main/java/dto/UserDTO.java
+++ src/main/java/dto/UserDTO.java
@@ -1,5 +1,37 @@
 package dto;
 
-public class UserDTO {
+import domain.User;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class UserDTO {
+	//도메인 필드에 맞게 수정
+//	private boolean likeStatement;
+//	private int userId;
+//	private int reviewId;
+//	
+//	@Builder
+//	public UserDTO(boolean likeStatement,int userId,int reviewId) {
+//		
+//		this.likeStatement = likeStatement;
+//		this.userId = userId;
+//		this.reviewId = reviewId;
+//	}
+//	
+//	public static Like toEntity(UserDTO userDTO) {
+//		return User.builder()
+//					.deptno(userDTO.getDeptno())
+//					.dname(userDTO.getDname())
+//					.loc(userDTO.getLoc())
+//					.build();
+//	}
 }
