4a3b40af280abf3594441bcb9d3cc7bd47579f14
wisdom <cocoa389@cau.ac.kr>
2024-05-26 03:35:49 +0900

    Test : Test code for RatingService
    CRUD 테스트 모두 성공

--- src/test/java/service/RatingServiceTest.java
+++ src/test/java/service/RatingServiceTest.java
@@ -8,5 +8,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class RatingServiceTest {
 
     private final RatingService ratingService = new RatingService();
-    
+    @Test
+    void insertRating() {
+        RatingDTO ratingDTO = RatingDTO.builder()
+                .ratingScore(5)
+                .build();
+
+        ratingService.insertRating(ratingDTO);
+        RatingDTO retrievedRating = ratingService.getRating(1);
+
+        assertNotNull(retrievedRating);
+        assertEquals(retrievedRating.getRatingId(), 1, "해당 평점은 id가 1이어야 함");
+    }
+
+    @Test
+    void getRating() {
+        RatingDTO ratingDTO = RatingDTO.builder()
+                .ratingScore(5)
+                .build();
+        ratingService.insertRating(ratingDTO);
+
+        RatingDTO retrievedRating = ratingService.getRating(1);
+
+        assertNotNull(retrievedRating);
+        assertEquals(retrievedRating.getRatingScore(), 5, "해당 평점은 점수가 5점이어야 함");
+    }
+
+    @Test
+    void deleteRating() {
+        RatingDTO ratingDTO = RatingDTO.builder()
+                .ratingScore(5)
+                .build();
+        ratingService.insertRating(ratingDTO);
+
+        ratingService.deleteRating(1);
+        assertThrows(NullPointerException.class, () -> {
+            ratingService.getRating(1);
+        }, "삭제 후 null이 될 것");
+
+    }
 }
