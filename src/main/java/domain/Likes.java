ff8ab00c1d06a4829dd8c36910520c7a6b5fa7ba
HYH <dihexyldecyl@gmail.com>
2024-05-27 20:58:54 +0900

    #Fix: 오류 수정

--- src/main/java/domain/Likes.java
+++ src/main/java/domain/Likes.java
@@ -1,7 +1,22 @@
 package domain;
 
-import javax.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
