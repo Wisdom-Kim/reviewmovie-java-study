0a533f4bd60ad6cc1b52cbcc93f0c68a97ebc486
HYH <dihexyldecyl@gmail.com>
2024-05-27 15:42:53 +0900

    #fix: JpaUtil 변경

--- src/main/java/util/JpaUtil.java
+++ src/main/java/util/JpaUtil.java
@@ -1,35 +1,36 @@
 package util;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class JpaUtil {
 	// persistence.xml 내의 persistence-unit tag name
-	 private static final String PERSISTENCE_UNIT = "jpa_config";
+	private static final String PERSISTENCE_UNIT = "jpa_config";
 
-	    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
 
-	    public static EntityManagerFactory getEntityManagerFactory() {
-	        return emf;
-	    }
+	public static EntityManagerFactory getEntityManagerFactory() {
+		return emf;
+	}
 
-	    public static EntityManager getEntityManager() {
-	        return getEntityManagerFactory().createEntityManager();
-	    }
+	public static EntityManager getEntityManager() {
+		return getEntityManagerFactory().createEntityManager();
+	}
 
-	    public static void closeEntityManager(EntityManager em) {
-	        if (em != null) {
-	            em.close();
-	        }
-	    }
+	public static void closeEntityManager(EntityManager em) {
+		if (em != null) {
+			em.close();
+		}
+	}
 
-	    public static void closeEntityManagerFactory() {
-	        if (emf != null) {
-	            emf.close();
-	        }
-	    }
+	public static void closeEntityManagerFactory() {
+		if (emf != null) {
+			emf.close();
+		}
+	}
 
-	    // 싱글톤으로 변경
-	    private JpaUtil() {}
+	// 싱글톤으로 변경
+//	private JpaUtil() {}
+	
 }
