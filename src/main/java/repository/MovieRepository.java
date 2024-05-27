4ca9bd4c249b29a1265185815baa78988573e54c
yeosujin <58133978+yeosujin@users.noreply.github.com>
2024-05-28 08:40:22 +0900

    #Feat : exception on MovieRepository.java

--- src/main/java/repository/MovieRepository.java
+++ src/main/java/repository/MovieRepository.java
@@ -16,50 +16,70 @@ import util.JpaUtil;
 
 public class MovieRepository {
 
-	//엔터티와 서비스 계층 사이 계층
-	//저장소 하나는 계속 쓰여야하므로 싱글톤으로 관리
-	private static final EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
-	private static MovieRepository instance;
+    private static final EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+    private static MovieRepository instance;
 
-	 private MovieRepository() {}
+    private MovieRepository() {}
 
-	 public static synchronized MovieRepository getInstance() {
-	     if (instance == null) {
-	         instance = new MovieRepository();
-	     }
-	     return instance;
-	 }
+    public static synchronized MovieRepository getInstance() {
+        if (instance == null) {
+            instance = new MovieRepository();
+        }
+        return instance;
+    }
 
-	 public Movie findById(int id,EntityManager em) {
-	     return em.find(Movie.class, id);
-	 }
+    public Movie findById(int id, EntityManager em) {
+        try {
+            return em.find(Movie.class, id);
+        } catch (Exception e) {
+            // 예외 처리 로직 추가
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	 public List<Movie> searchMoviesByTitle(String title, EntityManager em) {
-	     String jpql = "SELECT m FROM Movie m WHERE m.movieTitle LIKE :title";
-	     TypedQuery<Movie> query = em.createQuery(jpql, Movie.class);
-	     query.setParameter("title", "%" + title + "%");
-	     return query.getResultList();
-	 }
+    public List<Movie> searchMoviesByTitle(String title, EntityManager em) {
+        try {
+            String jpql = "SELECT m FROM Movie m WHERE m.movieTitle LIKE :title";
+            TypedQuery<Movie> query = em.createQuery(jpql, Movie.class);
+            query.setParameter("title", "%" + title + "%");
+            return query.getResultList();
+        } catch (Exception e) {
+            // 예외 처리 로직 추가
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
 
+    public List<Movie> findMoviesByRatingDesc(EntityManager em) {
+        try {
+            String jpql = "SELECT DISTINCT m "
+                    + "FROM Movie m "
+                    + "LEFT JOIN m.reviewList r "
+                    + "LEFT JOIN r.rating rat "
+                    + "ORDER BY (SELECT AVG(r2.rating.ratingScore) FROM Review r2 WHERE r2.movie = m) DESC";
+            TypedQuery<Movie> query = em.createQuery(jpql, Movie.class);
+            List<Movie> movies = query.getResultList();
+            return movies;
+        } catch (Exception e) {
+            // 예외 처리 로직 추가
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
 
-	 public List<Movie> findMoviesByRatingDesc(EntityManager em) {
-		    String jpql = "SELECT DISTINCT m "
-		            + "FROM Movie m "
-		            + "LEFT JOIN m.reviewList r "
-		            + "LEFT JOIN r.rating rat "
-		            + "ORDER BY (SELECT AVG(r2.rating.ratingScore) FROM Review r2 WHERE r2.movie = m) DESC";
-		    TypedQuery<Movie> query = em.createQuery(jpql, Movie.class);
-		    List<Movie> movies = query.getResultList();
-		    return movies;
-	 }
-
-	 public double getAverageRating(MovieDTO movieDTO, EntityManager em) {
-	     TypedQuery<Double> query = em.createQuery(
-	             "SELECT AVG(r.ratingScore) FROM Rating r WHERE r.review.movie.movieId = :movieId", Double.class
-	     );
-	     query.setParameter("movieId", movieDTO.getMovieId());
-	     Double averageRating = query.getSingleResult();
-	     return (averageRating != null) ? averageRating : 0.0;
-	 }
-
+    public double getAverageRating(MovieDTO movieDTO, EntityManager em) {
+        try {
+            TypedQuery<Double> query = em.createQuery(
+                    "SELECT AVG(r.ratingScore) FROM Rating r WHERE r.review.movie.movieId = :movieId", Double.class
+            );
+            query.setParameter("movieId", movieDTO.getMovieId());
+            Double averageRating = query.getSingleResult();
+            return (averageRating != null) ? averageRating : 0.0;
+        } catch (Exception e) {
+            // 예외 처리 로직 추가
+            e.printStackTrace();
+            return 0.0;
+        }
+    }
 }
