a4b2e9e664f7e30f98a9be1427c3ab5e1ea34846
wisdom <52325199+Wisdom-Kim@users.noreply.github.com>
2024-05-27 15:35:35 +0900

    TEST : sql 테스트데이터 삽입문 추가

--- src/main/java/util/JpaUtil.java
+++ src/main/java/util/JpaUtil.java
@@ -1,5 +1,6 @@
 package util;
 
+<<<<<<< Updated upstream
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -32,4 +33,37 @@ public class JpaUtil {
 
 	    // 싱글톤으로 변경
 	    private JpaUtil() {}
+=======
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtil {
+	// persistence.xml 내의 persistence-unit tag name
+	
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_config");
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return emf;
+    }
+
+    public static EntityManager getEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    public static void closeEntityManager(EntityManager em) {
+        if (em != null) {
+            em.close();
+        }
+    }
+
+    public static void closeEntityManagerFactory() {
+        if (emf != null) {
+            emf.close();
+        }
+    }
+
+    // 싱글톤으로 변경
+    private JpaUtil() {}
+>>>>>>> Stashed changes
 }
