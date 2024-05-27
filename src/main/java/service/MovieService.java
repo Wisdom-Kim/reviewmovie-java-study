bd50f281c0eff9f4ee0d10d31d309259625fb883
yeosujin <58133978+yeosujin@users.noreply.github.com>
2024-05-28 08:41:37 +0900

    #Clean : exception on MovieService.java

--- src/main/java/service/MovieService.java
+++ src/main/java/service/MovieService.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import domain.Movie;
-import domain.Review;
 import dto.MovieDTO;
-import dto.ReviewDTO;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -14,7 +12,8 @@ import repository.MovieRepository;
 import util.JpaUtil;
 
 public class MovieService {
-	private static MovieService instance;
+
+    private static MovieService instance;
     private static final EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
     private MovieRepository movieRepository;
 
@@ -32,91 +31,51 @@ public class MovieService {
     public List<MovieDTO> searchMoviesByTitle(String movieTitle) {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = null;
-        try {
-            tx = em.getTransaction();
-            tx.begin();
-
-            List<Movie> movies = movieRepository.searchMoviesByTitle(movieTitle, em);
-            List<MovieDTO> movieDTOs = movies.stream()
-                    .map(MovieDTO::fromEntity)
-                    .collect(Collectors.toList());
-
-            tx.commit();
-            return movieDTOs;
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
-        }
+        tx = em.getTransaction();
+        tx.begin();
+        List<Movie> movies = movieRepository.searchMoviesByTitle(movieTitle, em);
+        List<MovieDTO> movieDTOs = movies.stream()
+                .map(MovieDTO::fromEntity)
+                .collect(Collectors.toList());
+        tx.commit();
+        em.close();
+        return movieDTOs;
     }
 
     public List<MovieDTO> getMoviesByRatingDesc() {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = null;
-        try {
-            tx = em.getTransaction();
-            tx.begin();
-
-            List<Movie> movies = movieRepository.findMoviesByRatingDesc(em);
-            List<MovieDTO> movieDTOs = movies.stream()
-                    .map(MovieDTO::fromEntity)
-                    .collect(Collectors.toList());
-
-            tx.commit();
-            return movieDTOs;
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
-        }
+        tx = em.getTransaction();
+        tx.begin();
+        List<Movie> movies = movieRepository.findMoviesByRatingDesc(em);
+        List<MovieDTO> movieDTOs = movies.stream()
+                .map(MovieDTO::fromEntity)
+                .collect(Collectors.toList());
+        tx.commit();
+        em.close();
+        return movieDTOs;
     }
 
     public double getAverageRating(MovieDTO movieDTO) {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = null;
-        try {
-            tx = em.getTransaction();
-            tx.begin();
-
-            double averageRating = movieRepository.getAverageRating(movieDTO, em);
-
-            tx.commit();
-            return averageRating;
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
-        }
+        tx = em.getTransaction();
+        tx.begin();
+        double averageRating = movieRepository.getAverageRating(movieDTO, em);
+        tx.commit();
+        em.close();
+        return averageRating;
     }
 
     public MovieDTO getMovie(int movieId) {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = null;
-        try {
-            tx = em.getTransaction();
-            tx.begin();
-
-            Movie movie = movieRepository.findById(movieId, em);
-            MovieDTO movieDTO = MovieDTO.fromEntity(movie);
-
-            tx.commit();
-            return movieDTO;
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
-        }
+        tx = em.getTransaction();
+        tx.begin();
+        Movie movie = movieRepository.findById(movieId, em);
+        MovieDTO movieDTO = MovieDTO.fromEntity(movie);
+        tx.commit();
+        em.close();
+        return movieDTO;
     }
 }
